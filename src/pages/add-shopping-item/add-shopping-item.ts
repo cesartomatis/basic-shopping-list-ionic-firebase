e1cc2b98b8590cace97a2bf3f44e027cba382c5b
Cesar Tomatis <cesartomatis@outlook.com>
2018-05-12 22:57:04 -0300

    added ui for add item page

--- src/pages/add-shopping-item/add-shopping-item.ts
+++ src/pages/add-shopping-item/add-shopping-item.ts
@@ -11,15 +11,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-add-shopping-item',
-  templateUrl: 'add-shopping-item.html',
+  templateUrl: 'add-shopping-item.html'
 })
 export class AddShoppingItemPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddShoppingItemPage');
   }
-
+  onAddItem() {}
 }
